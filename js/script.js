143eb31cd2d8aefb0091e0245720a1239dd32587
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-13 17:42:44 +0100

    output on browser, just the different color to fix

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
+let containerElem = document.querySelector(".container")
+let boxClass;
 for (let i=1; i <= 100; i++) {
 
     if ((i%3 === 0) && (i%5 === 0)) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
+        boxText = "FizzBuzz";
     } else if (i%3 === 0) {
-        console.log("Fizz")
+        console.log("Fizz");
+        boxText = "Fizz";
     } else if (i%5 === 0) {
-        console.log("Buzz")
+        console.log("Buzz");
+        boxText = "Buzz";
     } else {
         console.log(i);
+        boxText = i;
     }
+
+    containerElem.innerHTML += `<div class="box">${boxText}</div>`
 }
